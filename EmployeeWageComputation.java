2cf5f267069e4b68fd15bd9e49bc069d63b5bd69
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-06-09 16:34:09 +0530

    Add: Maximum working days and hours condition added

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -5,38 +5,47 @@ public class EmployeeWageComputation {
 		final int isPresent=1;
 		final int isPartTime=2;
 		int wagePerHour=20;
-		int empWorkingHour;
+		int empWorkingHour=0;
 		int empDailyWage;
 		int workingDayPerMonth=20;
-		int empInitialWage=0;
 		int empMonthlyWage=0;
+		int totalWorkingHour=0;
+		int maximumWorkingHour=100;
 
 		System.out.println("Welcome to Employee Wage Computation Program...!!");
 
 		for(int i=1; i<=workingDayPerMonth; i++) {
-		int empCheck = (int)(Math.floor(Math.random() * 10)) % 3;
 
-		switch (empCheck) {
-		case isPresent: {
-			empWorkingHour =8;
-			System.out.println("Employee is Present");
+			totalWorkingHour = empWorkingHour + totalWorkingHour ;
+
+		if (i <= workingDayPerMonth && totalWorkingHour <= maximumWorkingHour)
+		{
+			int empCheck = (int)(Math.floor(Math.random() * 10)) % 3;
+
+			switch (empCheck) {
+			case isPresent: {
+				empWorkingHour =8;
+				System.out.println("Employee is Present");
+				break;
+			}
+
+			case isPartTime: {
+				empWorkingHour =4;
+				System.out.println("Employee is Part Time Present");
+				break;
+			}
+			default:
+				empWorkingHour =0;
+				System.out.println("Employee is Absent");
+			}
+
+			empDailyWage = wagePerHour * empWorkingHour ;
+			System.out.println("Daily Wage of Employee : " +empDailyWage);
+			empMonthlyWage = empDailyWage + empMonthlyWage ;
+
+		}
+		else
 			break;
-		}
-
-		case isPartTime: {
-			empWorkingHour =4;
-			System.out.println("Employee is Part Time Present");
-			break;
-		}
-		default:
-			empWorkingHour =0;
-			System.out.println("employee is Absent");
-		}
-
-		empDailyWage = wagePerHour * empWorkingHour ;
-		System.out.println("Daily Wage of Employee : " +empDailyWage);
-		empMonthlyWage = empDailyWage + empInitialWage ;
-		empInitialWage = empMonthlyWage ;
 
 	}
 		System.out.println("Monthly Wage of Employee : " +empMonthlyWage);
