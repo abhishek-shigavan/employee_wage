b183daf89d85ffcc80cfb984112a7e8f91d0b77e
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-06-09 13:05:51 +0530

    Add: Part Time Employee and Wage added

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,27 +1,32 @@
 public class EmployeeWageComputation {
 
-        public static void main(String[] args) {
+	public static void main(String[] args) {
 
-                int isPresent = 1;
-                int wagePerHour = 20;
-                int empWorkingHour;
+		int isPresent = 1;
+		int isPartTime =2;
+		int wagePerHour = 20;
+		int empWorkingHour;
 
-                System.out.println("Welcome to Employee Wage Computation Program...!!");
+		System.out.println("Welcome to Employee Wage Computation Program...!!");
 
-                int empCheck = (int)(Math.floor(Math.random() * 10)) % 2;
+		int empCheck = (int)(Math.floor(Math.random() * 10)) % 3;
 
-                if (empCheck == isPresent) {
-                        empWorkingHour = 8;
-                        System.out.println("Employee is Present");
-                }
-                else {
-                        empWorkingHour = 0;
-                        System.out.println("Employee is Absent");
-                }
+		if (empCheck == isPresent) {
+			empWorkingHour = 8;
+			System.out.println("Employee is Present");
+		}
+		else if (empCheck == isPartTime) {
+			empWorkingHour = 4;
+			System.out.println("Employee is Part Time Present");
+		}
+		else {
+			empWorkingHour = 0;
+			System.out.println("Employee is Absent");
+		}
 
-                int empDailyWage = wagePerHour * empWorkingHour ;
+		int empDailyWage = wagePerHour * empWorkingHour ;
 
-                System.out.println("Daily wage of Employee: "+empDailyWage);
-        }
+		System.out.println("Daily wage of Employee: "+empDailyWage);
+	}
 }
 
