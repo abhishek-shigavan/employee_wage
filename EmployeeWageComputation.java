6309976eebae2e8127b18064cdebaeae32f62f58
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-06-09 15:28:55 +0530

    Add: Calculating monthly wage of employee

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -2,13 +2,18 @@ public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
 
-		final int isPresent = 1;
-		final int isPartTime =2;
-		int wagePerHour = 20;
+		final int isPresent=1;
+		final int isPartTime=2;
+		int wagePerHour=20;
 		int empWorkingHour;
+		int empDailyWage;
+		int workingDayPerMonth=20;
+		int empInitialWage=0;
+		int empMonthlyWage=0;
 
 		System.out.println("Welcome to Employee Wage Computation Program...!!");
 
+		for(int i=1; i<=workingDayPerMonth; i++) {
 		int empCheck = (int)(Math.floor(Math.random() * 10)) % 3;
 
 		switch (empCheck) {
@@ -28,8 +33,12 @@ public class EmployeeWageComputation {
 			System.out.println("employee is Absent");
 		}
 
-		int empDailyWage = wagePerHour * empWorkingHour ;
+		empDailyWage = wagePerHour * empWorkingHour ;
+		System.out.println("Daily Wage of Employee : " +empDailyWage);
+		empMonthlyWage = empDailyWage + empInitialWage ;
+		empInitialWage = empMonthlyWage ;
 
-		System.out.println("Daily wage of Employee: "+empDailyWage);
+	}
+		System.out.println("Monthly Wage of Employee : " +empMonthlyWage);
 	}
 }
