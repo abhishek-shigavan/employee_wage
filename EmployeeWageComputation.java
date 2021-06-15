a25a8f617affdae18b2bb375d0e9f411bc18d143
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-06-15 16:06:31 +0530

    Refactor: Part Time Employee Added

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,32 +1,48 @@
+/**
+ * EmployeeWageComputation --- Program to check Employee is Present / 
+ * 			       Part Time / Absent & computing Daily Wage.
+ * 
+ * @author Abhishek Shigavan
+ *
+ */
 public class EmployeeWageComputation {
+	
+	//constant variables
+	public static final int IS_PRESENT =1;
+	public static final int IS_PART_TIME =2;
+	public static final int WAGE_PER_HOUR =20;
+/**
+ * Checking attendance & accordingly
+ * setting work hours.
+ * Computing Daily Wage as per work hours.
+ * Printing Daily Wage
+ * 
+ * @return No return	
+ */	
+    public static void main(String[] args) {
+    		//local variables
+    		int empWorkHours =0;
+    		
+            System.out.println("Welcome to Employee Wage Computation Program...!!");
 
-	public static void main(String[] args) {
-
-		int isPresent = 1;
-		int isPartTime =2;
-		int wagePerHour = 20;
-		int empWorkingHour;
-
-		System.out.println("Welcome to Employee Wage Computation Program...!!");
-
-		int empCheck = (int)(Math.floor(Math.random() * 10)) % 3;
-
-		if (empCheck == isPresent) {
-			empWorkingHour = 8;
-			System.out.println("Employee is Present");
-		}
-		else if (empCheck == isPartTime) {
-			empWorkingHour = 4;
-			System.out.println("Employee is Part Time Present");
-		}
-		else {
-			empWorkingHour = 0;
-			System.out.println("Employee is Absent");
-		}
-
-		int empDailyWage = wagePerHour * empWorkingHour ;
-
-		System.out.println("Daily wage of Employee: "+empDailyWage);
-	}
+            int empAttendance = (int)(Math.floor(Math.random() * 10)) % 3;
+            
+            //checking attendance
+            if (empAttendance == IS_PRESENT) {
+            	empWorkHours =8;
+                System.out.println("Employee is Present");
+            }
+            else if (empAttendance == IS_PART_TIME) {
+            	empWorkHours =4;
+            	System.out.println("Employee is Part Time Present");
+            }
+            else {
+            	empWorkHours =0;
+                System.out.println("Employee is Absent");
+            }
+            
+            //computing daily wage
+            int empDailyWage = empWorkHours * WAGE_PER_HOUR;
+            System.out.println("Daily Wage of Employee : "+empDailyWage);
+    }
 }
-
