8a70dde654cc8a766e5c2d4efc338a993c2aefd5
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-06-15 16:47:20 +0530

    Refactor: Computing Monthly Wage till Maximum working days / hours reached

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,53 +1,68 @@
+/**
+ * EmployeeWageComputation --- Program to check monthly attendance 
+ * 			       of employee & according to it computing Monthly Wage
+ * 
+ * @author Abhishek Shigavan
+ *
+ */
 public class EmployeeWageComputation {
-
-	public static void main(String[] args) {
-
-		final int isPresent=1;
-		final int isPartTime=2;
-		int wagePerHour=20;
-		int empWorkingHour=0;
-		int empDailyWage;
-		int workingDayPerMonth=20;
-		int empMonthlyWage=0;
-		int totalWorkingHour=0;
-		int maximumWorkingHour=100;
-
-		System.out.println("Welcome to Employee Wage Computation Program...!!");
-
-		for(int i=1; i<=workingDayPerMonth; i++) {
-
-			totalWorkingHour = empWorkingHour + totalWorkingHour ;
-
-		if (i <= workingDayPerMonth && totalWorkingHour <= maximumWorkingHour)
-		{
-			int empCheck = (int)(Math.floor(Math.random() * 10)) % 3;
-
-			switch (empCheck) {
-			case isPresent: {
-				empWorkingHour =8;
-				System.out.println("Employee is Present");
-				break;
+	
+	//constant variables
+	public static final int IS_PRESENT =1;
+	public static final int IS_PART_TIME =2;
+	public static final int WAGE_PER_HOUR =20;
+	public static final int WORKING_DAY_PER_MONTH =20;
+	public static final int MAX_WORKING_HOUR =100;
+/**
+ * Checking attendance & accordingly
+ * setting work hours.
+ * Ensuring Maximum working day / hour limit should not exceed. 
+ * Computing Monthly Wage as per total work hours.
+ * Printing Monthly Wage
+ * 
+ * @return No return	
+ */	
+    public static void main(String[] args) {
+    	//local variables
+    	int empWorkHours =0;
+    	int totalWorkingHour =0;
+    	
+    	System.out.println("Welcome to Employee Wage Computation Program...!!");
+    	
+    	for(int i=1; i <= WORKING_DAY_PER_MONTH; i++) {
+            
+    		//checking for maximum working day / hours limit
+		if (i <= WORKING_DAY_PER_MONTH && totalWorkingHour <= MAX_WORKING_HOUR ) {
+				
+			int empAttendance = (int)(Math.floor(Math.random() * 10)) % 3;
+            
+			//checking for attendance & setting work hour value
+			switch (empAttendance) {
+				case IS_PRESENT: {
+					empWorkHours =8;
+					break;
+				}
+				case IS_PART_TIME: {
+					empWorkHours =4;
+					break;
+				}
+				default: {
+					empWorkHours =0;
+					break;
+				}
 			}
-
-			case isPartTime: {
-				empWorkingHour =4;
-				System.out.println("Employee is Part Time Present");
-				break;
-			}
-			default:
-				empWorkingHour =0;
-				System.out.println("Employee is Absent");
-			}
-
-			empDailyWage = wagePerHour * empWorkingHour ;
-			System.out.println("Daily Wage of Employee : " +empDailyWage);
-			empMonthlyWage = empDailyWage + empMonthlyWage ;
-
+				
+			//adding working hours into total working hours
+			totalWorkingHour += empWorkHours;
 		}
-		else
+		else {
 			break;
-
-	}
-		System.out.println("Monthly Wage of Employee : " +empMonthlyWage);
-	}
+		}
+    	}
+            
+        //computing monthly wage
+        int empMonthlyWage = totalWorkingHour * WAGE_PER_HOUR;
+        System.out.println("Monthly Wage of Employee : "+empMonthlyWage);
+    }
 }
+
