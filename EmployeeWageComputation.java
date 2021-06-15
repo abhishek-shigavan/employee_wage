8a43dc98ee2e14914a26f4e43f9cc8d32ce447a6
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-06-15 16:00:25 +0530

    Refactor: Computing Daily Wage of Employee

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,27 +1,44 @@
+/**
+ * EmployeeWageComputation --- Program to check Employee is 
+ *                             Present / Absent & computing Daily Wage.
+ * 
+ * @author Abhishek Shigavan
+ *
+ */
 public class EmployeeWageComputation {
+	
+	//constant variables
+	public static final int IS_PRESENT =1;
+	public static final int WAGE_PER_HOUR =20;
+/**
+ * Checking attendance & accordingly
+ * setting work hours.
+ * Computing Daily Wage as per work hours.
+ * Printing Daily Wage
+ * 
+ * @return No return	
+ */	
+    public static void main(String[] args) {
+    	    //local variables
+    	    int empWorkHours =0;
+    		
+            System.out.println("Welcome to Employee Wage Computation Program...!!");
 
-        public static void main(String[] args) {
-
-                int isPresent = 1;
-                int wagePerHour = 20;
-                int empWorkingHour;
-
-                System.out.println("Welcome to Employee Wage Computation Program...!!");
-
-                int empCheck = (int)(Math.floor(Math.random() * 10)) % 2;
-
-                if (empCheck == isPresent) {
-                        empWorkingHour = 8;
-                        System.out.println("Employee is Present");
-                }
-                else {
-                        empWorkingHour = 0;
-                        System.out.println("Employee is Absent");
-                }
-
-                int empDailyWage = wagePerHour * empWorkingHour ;
-
-                System.out.println("Daily wage of Employee: "+empDailyWage);
-        }
+            int empAttendance = (int)(Math.floor(Math.random() * 10)) % 2;
+            
+            //checking attendance
+            if (empAttendance == IS_PRESENT) {
+            		empWorkHours =8;
+                    System.out.println("Employee is Present");
+            }
+            else {
+            		empWorkHours =0;
+                    System.out.println("Employee is Absent");
+            }
+            
+            //computing daily wage
+            int empDailyWage = empWorkHours * WAGE_PER_HOUR;
+            System.out.println("Daily Wage of Employee : "+empDailyWage);
+    }
 }
 
