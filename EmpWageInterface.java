c324526809661aa50a87c046204b2dc1974327f8
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-06-20 14:18:50 +0530

    Add: Defining Methods

---
+++ EmpWageInterface.java
@@ -0,0 +1,7 @@
+public interface EmpWageInterface {
+	
+	public void addCompanyEmpWage(String company_Name, int wage_Per_Hour, int working_Day_Per_Month, int max_Working_Hour);
+	
+	public void computeEmpWage();
+
+}
