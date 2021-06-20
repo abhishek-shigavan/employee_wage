0bcbd9149f7759da28aecdee8e3ea747d34f5e14
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-06-20 10:12:51 +0530

    Add: Using Instance Variables

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -10,6 +10,20 @@ public class EmployeeWageComputation {
 	//constant variables 
 	public static final int IS_PRESENT =1;
 	public static final int IS_PART_TIME =2;
+	//instance variables
+	private String company_Name;
+	private int wage_Per_Hour;
+	private int working_Day_Per_Month;
+	private int max_Working_Hour;
+	private int empMonthlyWage;
+	
+	public EmployeeWageComputation(String company_Name, int wage_Per_Hour, int working_Day_Per_Month,int max_Working_Hour) {
+		
+		this.company_Name = company_Name;
+		this.wage_Per_Hour = wage_Per_Hour;
+		this.working_Day_Per_Month = working_Day_Per_Month;
+		this.max_Working_Hour = max_Working_Hour;
+	}
 /**
  * This method checks attendance & accordingly
  * sets value of working hour.
@@ -20,10 +34,12 @@ public class EmployeeWageComputation {
  * 
  * @return No return	
  */
-	public static void getEmployeeWage(String company_Name, int wage_Per_Hour, int working_Day_Per_Month, int max_Working_Hour) {
+	public void getEmployeeWage() {
 		
 		//local variables
-		int empWorkHours =0, totalWorkingHour =0, totalWorkingDay =0;
+		int empWorkHours =0;
+		int totalWorkingHour =0;
+		int totalWorkingDay =0;
 	
 			//checking for maximum working day / hours limit
 			while(totalWorkingDay <= working_Day_Per_Month && totalWorkingHour <= max_Working_Hour ) 
@@ -51,22 +67,36 @@ public class EmployeeWageComputation {
 				totalWorkingHour += empWorkHours;
 			}
 		//computing monthly wage
-		int empMonthlyWage = totalWorkingHour * wage_Per_Hour;
-		System.out.println("Company : "+company_Name+"\n Monthly Wage of "+company_Name+" Employee : " +empMonthlyWage);
+		empMonthlyWage = totalWorkingHour * wage_Per_Hour;
+	}
+	
+	@Override
+	public String toString() {
+		return "EmployeeWageComputation [Company Name = " + company_Name + ", Monthly Wage Of Employee = " + empMonthlyWage + "]";
 	}
 /**
  * Prints out welcome message.
- * Calling getEmployeeWage method 
- * 
+ * Calling getEmployeeWage for multiple 
+ * companies using multiple object
+ *  
  * @return No return value.
- */
+ */	
 	public static void main(String[] args) {
-
+		
 		System.out.println("Welcome to Employee Wage Computation Program...!!");
-		//passing values for multiple companies
-		getEmployeeWage("D-mart", 30, 25, 100 );
-		getEmployeeWage("Reliance", 40, 25, 120 );
-		getEmployeeWage("Big-Bazar", 35, 22, 105 );
+		//creating object for multiple company
+		EmployeeWageComputation dMart = new EmployeeWageComputation("D-Mart", 30, 25, 100);
+		EmployeeWageComputation reliance = new EmployeeWageComputation("Reliance", 40, 25, 120);
+		EmployeeWageComputation bigBazar = new EmployeeWageComputation("Big Bazar", 35, 22, 105);
+		
+		dMart.getEmployeeWage();
+		System.out.println(dMart);
+		
+		reliance.getEmployeeWage();
+		System.out.println(reliance);
+		
+		bigBazar.getEmployeeWage();
+		System.out.println(bigBazar);
 	}
 }
 
